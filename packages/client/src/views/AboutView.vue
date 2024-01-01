0ba73e8647e92df6f3fadd568a06c84c528746fa
lkx <851657005@qq.com>
2024-01-01 11:49:31 +0800

    feat: 测试小程序go-2

--- packages/client/src/views/AboutView.vue
+++ packages/client/src/views/AboutView.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div @click="goback">goback</div>
   </div>
 </template>
-
+<script setup lang="ts">
+const goback = (): void => {
+  history.go(-2)
+}
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
